f533bc1609bcaf3978a0a4ecd1f52f5a6f6de0eb
Jaden Simmons <jadensimmons1803@gmail.com>
2026-08-09 21:44:15 -0400

    initial commit

---
+++ backend/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userScehma = new mongoose.Schema({
+  username: {
+    type: string,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: string,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: string,
+    required: true,
+  },
+
+});
+
+module.exports = mongoose.model('User', userScehma);
