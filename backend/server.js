de32fe998a958ca1bea5e83d784228335d844b3a
Jaden Simmons <jadensimmons1803@gmail.com>
2026-08-17 21:05:30 -0400

    established middleware

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
 const app = express();
 
+//Middleware
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
