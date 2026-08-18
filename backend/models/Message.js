937201d27ed64ad64c740273f585fb572c493795
Jaden Simmons <jadensimmons1803@gmail.com>
2026-08-17 21:49:50 -0400

    built server.js skeleton

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const messageSchema = new mongoose.Schema(
   {
@@ -30,4 +30,4 @@ const messageSchema = new mongoose.Schema(
 
 messageSchema.index({ conversation: 1, createdAt: 1 });
 
-module.exports = mongoose.model('Message', messageSchema);
+export default mongoose.model('Message', messageSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import mongoose from 'mongoose';
 
 const app = express();
 
-//Middleware
+const PORT = process.env.PORT || 5001
+
+// Middleware
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+
+// Connect to MongoDB
+try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected');
+    app.listen(PORT, () => console.log('On port ', PORT));
+} catch (err){
+    console.log(err);
+}
