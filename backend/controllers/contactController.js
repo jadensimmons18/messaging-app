d1405faaf91b17ffedcc7d8c9f4aedda3c097896
Jaden Simmons <jadensimmons1803@gmail.com>
2026-08-25 19:43:14 -0400

    added accept/reject friend and list requests

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -26,9 +26,9 @@ export const addFriend = async (req, res) => {
             return res.status(409).json({message: "Duplicate request"});
         }
 
-        const newContact = await Contact.create({requestedBy: userId, recipient});
+        const newContact = await Contact.create({requestedBy: userId, recipient: recipient});
         console.log("Success");
-        return res.status(201).json({message: "Contact created"});
+        return res.status(201).json({message: "Contact created", contact: newContact});
         
     } catch (err){
         console.error(err);
@@ -55,5 +55,70 @@ export const searchUser = async (req, res) => {
     } catch (err) {
         console.error(err);
         return res.status(500).json({message: 'Something went wrong'});
+    }
+}
+
+export const listRequests = async (req, res) => {
+    try {
+        // .populate() goes into the current users document 
+        const requests = await Contact.find({
+            recipient: req.userId,
+            status: 'pending',
+        }).populate('requestedBy', 'username avatarUrl');
+
+        return res.status(200).json({ message: 'Successful', requests });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Something went wrong' });
+    }
+};
+
+export const acceptFriend = async (req,res) => {
+    try {
+        const {id} = req.params; // Frontend will send over the document Id that the connection was established in
+        const contact = await Contact.findById(id); 
+        if (!contact) {
+            return res.status(404).json({message: 'Document not found'});
+        }
+        if (contact.recipient.toString() !== req.userId) {
+            return res.status(403).json({message: 'Recipient doesnt match user'});
+        }
+        if (contact.status !== 'pending') {
+            return res.status(409).json({message: 'Request either doesnt exist or was already accepted'});
+        }
+
+        contact.status = 'accepted';
+        await contact.save();
+
+        return res.status(200).json({message: 'Accepted'});
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({message: 'There was an issue accepting the request'});
+    }
+
+}
+
+export const rejectFriend = async (req,res) => {
+
+    try {
+        const {id} = req.params;
+        const contact = await Contact.findById(id);
+
+        if (!contact) {
+            return res.status(404).json({message: 'Document not found'});
+        }
+        if (contact.recipient.toString() !== req.userId) {
+                return res.status(403).json({message: 'Recipient doesnt match user'});
+        }
+        if (contact.status !== 'pending') {
+                return res.status(409).json({message: 'Request either doesnt exist or was already accepted'});
+        }
+
+        await contact.deleteOne();
+
+        return res.status(200).json({message: 'rejected successfully'})
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({message: 'There was an issue rejecting the request'});
     }
 }
--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import authMiddleware from '../middleware/authMiddleware.js';
-import {addFriend, searchUser} from '../controllers/contactController.js'
+import {addFriend, searchUser, acceptFriend, listRequests, rejectFriend} from '../controllers/contactController.js'
 
 const router = express.Router();
 
 router.post('/request', authMiddleware, addFriend);
 router.get('/search', authMiddleware, searchUser);
+router.get('/requests', authMiddleware, listRequests);
+router.patch('/:id/accept', authMiddleware, acceptFriend);
+router.delete('/:id/reject', authMiddleware, rejectFriend);
 
 export default router;
