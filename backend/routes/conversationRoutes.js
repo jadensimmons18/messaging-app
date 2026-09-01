572cf632b4f1802fe99362ea5dec23fb69c8e152
Jaden Simmons <jadensimmons1803@gmail.com>
2026-09-01 14:34:55 -0400

    Updated conversation router

--- backend/routes/conversationRoutes.js
+++ backend/routes/conversationRoutes.js
@@ -1,5 +1,9 @@
-import express from 'mongoose';
+import express from 'express';
+import authMiddleware from '../middleware/authMiddleware.js';
+import { getOrCreateConversation } from '../controllers/conversationController.js';
 
 const router = express.Router();
 
-router.post('')
+router.post('/', authMiddleware, getOrCreateConversation);
+
+export default router;
