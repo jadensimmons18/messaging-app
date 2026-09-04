7d4eb0885241c552d63271d0f26ad583bb5198c1
Jaden Simmons <jadensimmons1803@gmail.com>
2026-09-03 20:48:35 -0400

    loadMessageHistory almost complete

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,12 +1,38 @@
 import Conversation from "../models/Conversation.js";
+import Message from "../models/Message.js";
 
 
 export const loadMessageHistory = async (req, res) => {
-    const {conversationId} = req.paramms;
+    const {conversationId} = req.params;
     try {
         // Find the conversation
         const conversation = await Conversation.findById(conversationId);
+
+        if (!conversation) {
+            return res.status(404).json({message: 'Conversation doesnt exist'});
+        }
+
+        // Checks if the user making the request is in the conversation
+        // .some will call the function inside of it for each element in the array, 
+        // automatically passing in the current element as a parameter of the function
+        if (!conversation.participants.some((p) => p.toString() === req.userId)) {
+            return res.status(403).json({message: 'You are not authorized to make this request'});
+        }
+
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 20;
+
+        const messages = await Message.find({ conversation: conversationId })
+        .sort({ createdAt: -1 })
+        .skip((page - 1) * limit)
+        .limit(limit)
+        .populate('sender', 'username avatarUrl');
+
+        const total = await Message.countDocuments({ conversation: conversationId });
+        return res.status(200).json({message: 'Success'});
+
     } catch (err) {
+        console.error(err);
         return res.status(500).json({message: 'Something went wrong'});
     }
     
--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import authMiddleware from '../middleware/authMiddleware.js';
-import loadMessageHistory from '../controllers/messageController.js';
+import {loadMessageHistory} from '../controllers/messageController.js';
 
 const router = express.Router();
 
-router.get('/loadMessageHistory', authMiddleware, loadMessageHistory);
+router.get(':conversationId', authMiddleware, loadMessageHistory);
+
+export default router;
